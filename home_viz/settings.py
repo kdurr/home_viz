b3475b8f7d55fcaa440332c861682a0c37434938
kdurr <kristina.l.durr@gmail.com>
2016-07-08 12:00:56 -0400

    Add postgresql db

--- home_viz/settings.py
+++ home_viz/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'home_viz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'home_viz',
+        'USER': 'admin',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
     }
 }
 
