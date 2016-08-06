2d2023a8578b625481cb30673896d30e43127b40
kdurr <kristina.l.durr@gmail.com>
2016-08-06 15:59:32 -0400

    Fix time format

--- tweets/collect_tweets.py
+++ tweets/collect_tweets.py
@@ -2,9 +2,6 @@ from tweets.models import Phrase, Tweet
 from home_viz import twitter
 
 import time
-import datetime
-# NOTE: remove warnings
-# from django.utils import timezone
 
 def collect_tweets():
     phrases = Phrase.objects.all()
@@ -17,14 +14,13 @@ def collect_tweets():
             save_tweets(phrase, result)
 
 def save_tweets(phrase, tweet_data):
-    tweet_time = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(string_date,'%a %b %d %H:%M:%S +0000 %Y'))
-    formatted_date = datetime.datetime.strptime(tweet_time, "%Y-%m-%d %H:%M:%S.%f")
+    formatted_date = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(tweet_data['created_at'],'%a %b %d %H:%M:%S +0000 %Y'))
 
     tweet_exists = Tweet.objects.filter(phrase = phrase, tweet_text = tweet_data['text'], tweet_date = formatted_date).exists()
 
     if tweet_exists == False:
         Tweet(
-            phrase = phrase_id,
+            phrase = phrase,
             tweet_text = tweet_data['text'],
             tweet_location = tweet_data['coordinates'] + ' ' + tweet_data['user']['location'],
             tweet_date = formatted_date
