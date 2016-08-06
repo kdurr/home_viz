9a14b3898b3dc73ca0c8b10ac2f8d571b3fa0ff5
kdurr <kristina.l.durr@gmail.com>
2016-08-06 12:37:59 -0400

    Add Twython

--- home_viz/__init__.py
+++ home_viz/__init__.py
@@ -2,3 +2,12 @@ from __future__ import absolute_import
 
 from .celery import app as celery_app
 from twython import Twython
+
+APP_KEY = 'NELO3vTdiseeZBqvarS6ETbgL'
+APP_SECRET = '30WyVSqCwWcjNoq17JbV9aIJNEC2Z0ky4JuNd5pUAlg503gROy'
+
+twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+
+ACCESS_TOKEN = twitter.obtain_access_token()
+
+twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
