67e04281f3eaeb810d5c8a88fcf95ca9dadf2850
kdurr <kristina.l.durr@gmail.com>
2016-08-06 15:45:50 -0400

    Set up celery and worker

---
+++ tweets/tasks.py
@@ -0,0 +1,18 @@
+from celery.task.schedules import crontab
+from celery.decorators import periodic_task
+from celery.utils.log import get_task_logger
+
+from tweets.collect_tweets import collect_tweets
+
+logger = get_task_logger(__name__)
+
+@periodic_task(
+    run_every=(crontab(minute='*/1')),
+    name='pull_tweets',
+    ignore_result=True
+)
+
+def task_pull_tweets():
+    """pings twitter and saves tweets to database"""
+    collect_tweets()
+    logger.info("Pinged Twitter")
