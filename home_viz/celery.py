1b9b83599282f0c048d28c87bad5cf07494f4e78
kdurr <kristina.l.durr@gmail.com>
2016-08-15 11:03:57 -0400

    Try different syntax

--- home_viz/celery.py
+++ home_viz/celery.py
@@ -12,8 +12,8 @@ app = Celery('home_viz')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.conf.update(
-    BROKER_URL=os.environ.get('REDIS_URL'),
-    CELERY_RESULT_BACKEND=os.environ.get('REDIS_URL')
+    BROKER_URL=os.environ['REDIS_URL'],
+    CELERY_RESULT_BACKEND=os.environ['REDIS_URL']
 )
 
 
